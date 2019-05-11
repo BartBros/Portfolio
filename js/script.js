976d26e6ce6e2a478dff08220e5adc18ad20d9a1
BartBros <bb.aambroziak@gmail.com>
2019-05-11 16:05:03 +0200

    format js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-$card = $('.card');
-$cardBody = $('.card-body');
+const $card = $('.card');
+const $cardBody = $('.card-body');
 
 $($cardBody).hide();
 
 $($card).mouseenter(function(event){
-    $(event.target).children('.card-body').show();
-  });
+  $(event.target).children('.card-body').show();
+});
 
-  $($card).mouseleave(function(event){
-    $(event.target).children('.card-body').hide();
-  });
+$($card).mouseleave(function(event){
+  $(event.target).children('.card-body').hide();
+});
