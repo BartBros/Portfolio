952e819e4875cb25a4ddc2acaadd828ed5b2cc60
BartBros <bb.aambroziak@gmail.com>
2019-05-23 13:26:15 +0200

    latest version

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ $($cardBody).hide();
 
 $(document).ready(function(){
   $($card).on('mouseenter', function(event){
-    $(this).children( $cardBody ).show(2000);
+    $(this).children( $cardBody ).show();
   });
 });
 $($card).on('mouseleave', function(event){
-  $cardBody.hide(2000);
+  $cardBody.hide();
 });
