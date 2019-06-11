2821a4d6a654096fb6970a51fbc2d48273a8d646
BartBros <bb.aambroziak@gmail.com>
2019-06-11 11:10:59 +0200

    v2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
 const $card = $('.card');
 const $cardBody = $('.cb-hidden');
+const $projectInfo = $('.project-info');
+const $projectColumn = $('.project-column');
 
 $($cardBody).hide();
+$($projectInfo).hide();
 
-$(document).ready(function(){
-  $($card).on('mouseenter', function(event){
-    $(this).children( $cardBody ).show();
+$(document).ready(function () {
+  $($card).on('mouseenter', function (event) {
+    $(this).children($cardBody).show();
+  });
+  $($card).on('mouseleave', function (event) {
+    $cardBody.hide();
+  });
+  $($projectColumn).on('mouseenter', function (event) {
+    $(this).children($projectInfo).show();
+  });
+  $($projectColumn).on('mouseleave', function (event) {
+    $($projectInfo).hide();
   });
 });
-$($card).on('mouseleave', function(event){
-  $cardBody.hide();
-});
 
 
-$('.carousel').carousel({
-  interval: false
-})
 
-$(document).on("click","#next",function(e){
+// $(document).on("click","#next",function(e){
+//   e.preventDefault();
+//   var id = $(this).attr("href"),
+//       topSpace = 30;
+// //alert(id);
+//   $('html, body').animate({
+//     scrollTop: $(id).offset().top - topSpace
+//   }, 800);
+// });
+
+$(document).on("click", "#next", function (e) {
   e.preventDefault();
-  var id = $(this).attr("href"),
-      topSpace = 30;
-//alert(id);
+  var id = $(this).attr("href")
+  //alert(id);
   $('html, body').animate({
-    scrollTop: $(id).offset().top - topSpace
+    scrollTop: $(id).offset().top
   }, 800);
 });
